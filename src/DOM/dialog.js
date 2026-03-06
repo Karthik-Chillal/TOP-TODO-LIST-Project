91c448d8361ff883d8f3128cfc0c5f76e9438810
KarthikChillal <chillalkarthik@gmail.com>
2026-03-06 16:20:14 +0530

    dialog is fully functional now.

--- src/DOM/dialog.js
+++ src/DOM/dialog.js
@@ -1,4 +1,13 @@
 
 
 const dialog = document.querySelector(".tasks-dialog");
-dialog.showModal();
+const addTaskBtn = document.querySelectorAll(".add-task-btn");
+addTaskBtn.forEach((btn)=>{
+  btn.addEventListener("click" , ()=>{
+    dialog.showModal();
+  })
+});
+const cancelBtn = document.querySelector(".form-cancel-btn");
+cancelBtn.addEventListener("click", ()=>{
+  dialog.close();
+})
