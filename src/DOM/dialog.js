d21bb7f6e0402a06cf3b3ef0a2c6fee89fd8eeed
KarthikChillal <chillalkarthik@gmail.com>
2026-03-06 16:44:57 +0530

    input is stored in dom elements.

--- src/DOM/dialog.js
+++ src/DOM/dialog.js
@@ -1,6 +1,7 @@
 
 
 const dialog = document.querySelector(".tasks-dialog");
+const form = dialog.querySelector("form");
 const addTaskBtn = document.querySelectorAll(".add-task-btn");
 addTaskBtn.forEach((btn)=>{
   btn.addEventListener("click" , ()=>{
@@ -10,4 +11,24 @@ addTaskBtn.forEach((btn)=>{
 const cancelBtn = document.querySelector(".form-cancel-btn");
 cancelBtn.addEventListener("click", ()=>{
   dialog.close();
+})
+
+
+// input values here:
+form.addEventListener("submit", (e)=>{
+  e.preventDefault();
+  try {
+    const title = form.querySelector("#task-title").value;
+    const desc = form.querySelector("#task-desc").value;
+    const dueDate = form.querySelector("#task-due-date").value;
+    const checked = form.querySelector("input[name='priority']:checked");
+    const priority = checked ? checked.value : null;
+    console.log({title, desc, dueDate, priority});
+  }
+  catch(err){
+    console.log(err);
+  }
+  dialog.close();
+
+
 })
